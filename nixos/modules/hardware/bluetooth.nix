7ad66f356dee71e032629a7ead5b48a5c806e31f
0kenx <km@nxfi.app>
2025-05-14 01:59:07 +0200

    bt
    Signed-off-by: 0kenx <km@nxfi.app>

--- nixos/modules/hardware/bluetooth.nix
+++ nixos/modules/hardware/bluetooth.nix
@@ -2,10 +2,31 @@
 
 {
   # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    # Set input profile as headset for better compatibility
+    settings = {
+      General = {
+        ControllerMode = "dual";
+        # Prevent bluez from disconnecting devices when suspend
+        AutoEnable = true;
+        # Improve compatibility with some devices
+        FastConnectable = true;
+        # Better input device handling
+        JustWorksRepairing = "always";
+        Experimental = true;
+      };
+    };
+  };
+
+  # Fix for Hyprland crashes when using bluetoothctl
+  services.blueman.enable = true;
 
   environment.systemPackages = with pkgs; [
     overskride
+    bluez
+    bluez-tools
+    blueman
   ];
 }
--- user/.config/fish/functions/bluetooth_toggle.fish
+++ user/.config/fish/functions/bluetooth_toggle.fish
@@ -1,11 +1,19 @@
 function bluetooth_toggle
+    # Use blueman-manager instead of bluetoothctl to avoid Hyprland crash
     set bluetooth_status (rfkill list bluetooth | grep -i -o "Soft blocked: yes")
     set backup_file ~/.cache/airplane_backup
 
     if [ -z "$bluetooth_status" ]
         rfkill block bluetooth
+        notify-send "Bluetooth" "Bluetooth turned off" --icon=bluetooth-disabled
     else
         rfkill unblock bluetooth
+        # Wait a moment for bluetooth to initialize
+        sleep 0.5
+        # Restart bluetooth service to ensure clean state
+        systemctl --user restart blueman-applet
+        notify-send "Bluetooth" "Bluetooth turned on" --icon=bluetooth
+
         if test -e $backup_file
             rm $backup_file
         end
