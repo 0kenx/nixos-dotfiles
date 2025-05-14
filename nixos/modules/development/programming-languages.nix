4f96b412b0f9a87a2cb45fd36af84bfceb1eb160
0kenx <km@nxfi.app>
2025-05-14 11:24:19 +0200

    minor
    Signed-off-by: 0kenx <km@nxfi.app>

--- nixos/modules/development/programming-languages.nix
+++ nixos/modules/development/programming-languages.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    devenv
     go
     (python312Full.withPackages(ps: with ps; [ pygobject3 gobject-introspection pyqt6-sip]))
     nodePackages_latest.nodejs
