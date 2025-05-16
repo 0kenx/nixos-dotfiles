53d5cf885b22f09cf270d54a28212739a1580b1f
0kenx <km@nxfi.app>
2025-05-16 03:14:39 +0200

    minor
    Signed-off-by: 0kenx <km@nxfi.app>

--- nixos/modules/system/environment-variables.nix
+++ nixos/modules/system/environment-variables.nix
@@ -2,6 +2,9 @@
 
 {
   # Setup Env Variables
-  environment.variables.JDK_PATH = "${pkgs.jdk11}/";
-  environment.variables.NODEJS_PATH = "${pkgs.nodePackages_latest.nodejs}/";
+  environment.variables = {
+    JDK_PATH = "${pkgs.jdk11}/";
+    NODEJS_PATH = "${pkgs.nodePackages_latest.nodejs}/";
+    NIXOS_OZONE_WL = "1"; # Enable Wayland for Electron/CEF apps
+  };
 }
