b7b267ecce96b4c14edf17102dfae172073042c0
0kenx <km@nxfi.app>
2025-09-02 21:49:02 +0200

    update
    Signed-off-by: 0kenx <km@nxfi.app>

--- nixos/modules/development/cad.nix
+++ nixos/modules/development/cad.nix
@@ -5,7 +5,6 @@
   environment.systemPackages = with pkgs; [
     freecad-wayland
     orca-slicer
-    # Temporarily disable bambu-studio due to CUDA build issues
-    # bambu-studio
+    bambu-studio
   ];
 }
