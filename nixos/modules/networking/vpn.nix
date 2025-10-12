4ef859418ae44b28beaeac1b7395989b49f57b96
0kenx <km@nxfi.app>
2025-10-12 21:20:04 +0200

    add clash
    Signed-off-by: 0kenx <km@nxfi.app>

--- nixos/modules/networking/vpn.nix
+++ nixos/modules/networking/vpn.nix
@@ -1,9 +1,21 @@
 { pkgs, ... }:
 
+let
+  # Wrap clash-nyanpasu with proper Wayland/EGL environment variables
+  clash-nyanpasu-wrapped = pkgs.symlinkJoin {
+    name = "clash-nyanpasu";
+    paths = [ pkgs.clash-nyanpasu ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/clash-nyanpasu \
+        --set WEBKIT_DISABLE_DMABUF_RENDERER 1 \
+        --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+    '';
+  };
+in
 {
-
   environment.systemPackages = with pkgs; [
     tor-browser
-    clash-nyanpasu  # Clash GUI client based on tauri
+    clash-nyanpasu-wrapped  # Clash GUI client (wrapped for Wayland compatibility)
   ];
 }
